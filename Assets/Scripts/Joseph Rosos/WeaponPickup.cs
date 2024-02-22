0179461cf4b1b5d4dea8a4128fe6c07eceb076aa
joros501 <joseph.rosos06@gmail.com>
2024-02-22 11:58:31 -0600

    Added WeaponPickup Script(not done)

---
+++ Assets/Scripts/Joseph Rosos/WeaponPickup.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpController : MonoBehaviour
+{
+    public Weapon weapon;
+    
+    
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if(target.tag == "Player")
+        {
+            target.GetComponent<PlayerrController>(). Weapon = Weapon;
+            target.transform.GetChild(1).GetComponent<SpriteRenderer>().sprite = Weapon.currentWeaponSpr;
+            Destroy(gameObject);
+        }
+    }
+}
