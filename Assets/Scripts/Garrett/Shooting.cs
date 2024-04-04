8a0e439925fe085d0daa07621b80e1d89180ad97
joros501 <157503559+joros501@users.noreply.github.com>
2024-04-04 11:54:05 -0500

    Finished Fire Rate Scripts

--- Assets/Scripts/Garrett/Shooting.cs
+++ Assets/Scripts/Garrett/Shooting.cs
@@ -8,9 +8,7 @@ public class Shooting : MonoBehaviour
     public GameObject bulletPrefab;
     public float fireForce = 20f;
     public int currentClip, maxClipSize = 10, currentAmmo, maxAmmoSize = 100, ammoAmount;
-    public float fireRate = 1f;
-    public float shootDelay = 1000f; // Delay after shooting before next shot is allowed
-    private float nextFireTime = 1000f;
+    public float shootDelay = 0.5f; // Short delay after shooting before next shot is allowed
     private bool canShoot = true; // Flag to check if the gun can shoot
 
     public float bulletForce = 1000f;
@@ -40,30 +38,9 @@ public class Shooting : MonoBehaviour
 
     public void Fire()
     {
-        if (Time.time >= nextFireTime && currentClip > 0)
+        if (canShoot && currentClip > 0)
         {
-            GameObject bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
-            bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
-            currentClip--;
-            nextFireTime = Time.time + 1f / fireRate;
-            nextFireTime += shootDelay;
-        }
-    }
-
-    public void Reload()
-    {
-        int reloadAmount = maxClipSize - currentClip; // how many bullets to refill clip
-        reloadAmount = Mathf.Min(reloadAmount, currentAmmo); // Reload with available ammo
-        currentClip += reloadAmount;
-        currentAmmo -= reloadAmount;
-    }
-
-    public void AddAmmo(int newAmount)
-    {
-        currentAmmo += newAmount;
-        if (currentAmmo > maxAmmoSize)
-        {
-            currentAmmo = maxAmmoSize;
+            StartCoroutine(ShootWithDelay());
         }
     }
 }
