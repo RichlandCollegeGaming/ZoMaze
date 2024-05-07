1d4820cc5852b4262d7be137e0a12c4e84409f2e
Garrett Atwood <atwood.garrett@gmail.com>
2024-05-06 20:30:26 -0500

    updated some stuff

--- Assets/Scripts/Garrett/DestroyPickUp.cs
+++ Assets/Scripts/Garrett/DestroyPickUp.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class DestroyPickUp : MonoBehaviour
 {
+
+    public GameObject SoundPrefab;
+    public Transform PickUpPoint;
     private void OnTriggerEnter2D(Collider2D other)
     {
         // Check if the collider is the player
         if (other.CompareTag("Player"))
         {
+            GameObject Audio = Instantiate(SoundPrefab, PickUpPoint.position, PickUpPoint.rotation);
             Destroy(gameObject);
         }
     }
